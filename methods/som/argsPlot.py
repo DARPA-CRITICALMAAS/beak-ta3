22c525473a92048caba48693f143f96cf9e582cf
Ina Storch <98956042+Storch22@users.noreply.github.com>
2023-10-31 12:26:07 +0100

    argsPlot.py and plot.py
    Arguments for drawing plots from self organizing maps
    
    plot.py file is just created, not  filled with functionality

---
+++ methods/som/argsPlot.py
@@ -0,0 +1,13 @@
+class Args:
+#-- Arguments for drawing plots from self organizing maps
+
+    outsomfile: str     # dest="outsomfile", help='Som calculation somspace output text file')
+    som_x: int          # som x dimension
+    som_y:int           # som y dimension
+    input_file: str     # Input file(*.lrn)
+    dir: str            # Input file(*.lrn)
+    grid_type= 'square' # grid type (square or hexa)
+    redraw='true'       # whether to draw all plots, or only those required for clustering (true: draw all. false:draw only for clustering).
+    outgeofile=None     # som geospace results txt file
+    dataType=None       # Data type (scatter or grid)
+    noDataValue='NA'    # noData value
