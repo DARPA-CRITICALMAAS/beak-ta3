3cda3676226bc197fcb0d27831d9b1e3f86537c1
Ina Storch <98956042+Storch22@users.noreply.github.com>
2024-01-23 16:20:41 +0100

    add som

---
+++ src/beak/methods/som/load_geotiff.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 09:46:21 2019
+Script for reading geotiff input data files.
+@author: shautala
+"""
+
+import numpy as np
+from osgeo import gdal
+gdal.DontUseExceptions()  # gdal.UseExceptions()  # Explicitly Use or Don't Use Exceptions
+import os
+import sys
+
+def load_geotiff_files(input_file_list):
+    
+    width_0 = None
+    height_0 = None
+    gt_0 = None
+    prj_0=None
+    path_0=None
+    noDataValue_0 = None
+    dataType_0 = None
+    geotiff_list_as_string=input_file_list # At this points the assumption is made that the coordinates of separate files are identical. So the coordinates can just be taken from any one of the individual files.
+    returndata = np.array([])  
+    colnames=[]
+
+    if("," in geotiff_list_as_string):
+        geotiff_list_2=geotiff_list_as_string.split(",")#geotiff_list is str. geotiff_list_2 is array #maybe change the parameter name...?
+    else:
+        geotiff_list_2=[geotiff_list_as_string]
+
+    for geotiffpath in geotiff_list_2:   
+        src_ds = gdal.Open(geotiffpath, gdal.GA_ReadOnly)
+        band=src_ds.GetRasterBand(1)
+        noDataValue = band.GetNoDataValue()
+        dataType = band.DataType  
+        width = src_ds.RasterXSize
+        height = src_ds.RasterYSize
+        gt = src_ds.GetGeoTransform()
+        prj=src_ds.GetProjection()
+
+        if(width_0 is None):        #stash baseline values on first loop, for checking that projection etc. match for all files
+            width_0 = width
+            height_0 = height
+            gt_0 = gt
+            prj_0=prj
+            path_0=geotiffpath
+            noDataValue_0 = noDataValue
+            dataType_0 = dataType
+
+        if(gt!=gt_0):
+            print("Warning: Geotransform of "+geotiffpath+" does not match with "+path_0)
+        if(prj!=prj_0):
+            print("Warning: Projection of "+geotiffpath+" does not match with "+path_0)
+        if(width!=width_0 or height!=height_0):
+            sys.exit("Error: Grid of "+geotiffpath+" does not match."+path_0)
+        #if noDataValue != noDataValue_0:
+        #    print("Warning: noDataValue of " + geotiffpath + " does not match with " + path_0)
+        #if dataType != dataType_0:
+        #    print("Warning: dataType of " + geotiffpath + " does not match with " + path_0)
+
+    	# Read the raster data as a NumPy array
+        raster_array = src_ds.ReadAsArray()
+
+        # Set NoData values to np.nan
+        raster_array = np.where(raster_array == noDataValue, np.nan, raster_array)
+
+        # Check if data type is integer and convert to np.float32
+        if raster_array.dtype.kind == 'i':
+            raster_array = raster_array.astype(np.float32)
+
+        # Close the dataset
+        src_ds = None
+
+        flat=raster_array.flatten(order='C')
+        
+        # Use np.concatenate to stack arrays
+        returndata = np.concatenate([returndata, flat]) if returndata.size else flat
+
+        colnames.append(os.path.basename(geotiffpath))     
+
+    # Reshape returndata to have each raster as a column
+    returndata = returndata.reshape(-1, len(geotiff_list_2), order='F')
+    rows=len(returndata) 
+
+    if("," in geotiff_list_as_string):
+        cols=len(returndata[0])    
+    else:
+        cols=1            
+
+    return {'rows': rows, 'cols': cols, 'colnames': colnames, 
+            'headerlength': 0, 'data': returndata, 'filetype': 'geotiff','originaldata':raster_array, 
+            'geotransform':gt, 'noDataValue':np.nan, 'dataType':dataType
+            }   
+
+def delete_rows_with_no_data(geotiff_header):
+    
+    data = geotiff_header['data']
+    #originaldata = geotiff_header['originaldata']
+    #noDataValue = geotiff_header['noDataValue']
+
+    # Identify rows with noDataValue
+    rows_to_delete = np.any(np.isnan(data), axis=1)
+
+    # Delete rows with noDataValue
+    data_filtered = data[~rows_to_delete]
+
+    # Update the rows count
+    rows = len(data_filtered)
+
+    return {'rows': rows, 'cols': geotiff_header['cols'], 'colnames': geotiff_header['colnames'],
+            'headerlength': 0, 'data_all': geotiff_header['data'], 'data': data_filtered, 'filetype': 'geotiff', 'originaldata': geotiff_header['originaldata'],
+            'geotransform': geotiff_header['geotransform'], 'noDataValue': geotiff_header['noDataValue'], 'dataType': geotiff_header['dataType']
+            }
+
+def read_geotiff_coordinate_columns(geotiff_header):
+    coordinates_x=[]
+    coordinates_y=[]
+    coordinates_deleted = []         
+    data=geotiff_header['originaldata']
+    gt=geotiff_header['geotransform']   # gt[0]: x upper left, gt[1]: dx, gt[2]: row rotation (typically =0)
+                                        # gt[3]: y upper left, gt[4]: column rotation (typically =0), gt[5]: dy (negative value for a north-up image) 
+    
+    for i in range(0,len(data)):
+        for j in range(0,len(data[i])):
+            coordinates_x.append(j*gt[1]+gt[0])
+            coordinates_y.append(i*gt[5]+gt[3])
+    coordinates_all=np.column_stack((coordinates_x,coordinates_y))
+
+    colnames = ['X', 'Y']
+
+    if('data_all' in geotiff_header):        
+        # Identify rows with noDataValue
+        rows_to_delete = np.any(np.isnan(geotiff_header['data_all']), axis=1)
+
+        # Delete rows with noDataValue
+        coordinates = coordinates_all[~rows_to_delete]
+        coordinates_deleted = coordinates_all[rows_to_delete]
+    else:  
+        coordinates = coordinates_all
+
+    return {'data': coordinates, 'data_deleted': coordinates_deleted, 'data_all':coordinates_all,'colnames': colnames, 'fmt': '%f %f'} 
+
+def read_geotiff_data_columns(geotiff_header):
+    return {'data': geotiff_header['data'], 'colnames': geotiff_header['colnames'], 'fmt': ('%f ' * geotiff_header['cols']).rstrip()} 
+
+
+
