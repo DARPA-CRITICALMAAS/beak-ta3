5c9f019f7f06462ae8ce7c0b7a8d60ce58b731bb
Michael Steffen <michael.steffen@beak.de>
2024-05-27 13:06:56 +0200

    Hackathon 9M Updates

--- src/beak/models/mvt_reg.py
+++ src/beak/models/mvt_reg.py
@@ -9,4 +9,76 @@ models = {
         "Geology_Lithology_Minority": True,
         "...": False,
     },
+    "BASELINE_BISON": {
+        # Magnetics
+        "CEUS_MAG_TMAG_AAS_CEUSSSC_R0_Log": True,
+        "CEUS_MAG_DRTP_TDR_CEUSSSC_R0": True,
+        "CEUS_MAG_DRTP_HD_TDR_CEUSSSC_R0": True,
+        "CEUS_MAG_DRTP_CEUSSSC_R0": True,
+        "Mag_RTP_HGM_DeepSources": True,
+        "DeepMagSources_Worms_Proximity": True,
+        # Gravity
+        "CEUS_GRAV_RI_HD_1VD_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_HD_CEUSSSC_R0": True,
+        "CEUS_GRAV_Isostatic_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_1VD_CEUSSSC_R0": True,
+        # Seismics
+        "LAB": True,
+        "LAB_HGM": True,
+        "LAB_Worms_Proximity": True,
+        "Moho": True,
+    },
+    "FEATURE_FOX": {
+        # Magnetics
+        "CEUS_MAG_TMAG_AAS_CEUSSSC_R0_Log": True,
+        "CEUS_MAG_DRTP_TDR_CEUSSSC_R0": True,
+        "CEUS_MAG_DRTP_HD_TDR_CEUSSSC_R0": True,
+        "CEUS_MAG_DRTP_CEUSSSC_R0": True,
+        "Mag_RTP_HGM_DeepSources": True,
+        "DeepMagSources_Worms_Proximity": True,
+        # Gravity
+        "CEUS_GRAV_RI_HD_1VD_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_HD_CEUSSSC_R0": True,
+        "CEUS_GRAV_Isostatic_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_1VD_CEUSSSC_R0": True,
+        # Seismics
+        "LAB": True,
+        "LAB_HGM": True,
+        "LAB_Worms_Proximity": True,
+        "Moho": True,
+        # Geology
+        "distance_to_geology_macrostrat": True,
+        "distance_to_faults_macrostrat": True,
+        "cambrian-to-early-ordovician-carbonates_zero_macrostrat": True,
+        # Things to wait on: Geochemistry
+        "...": False,
+    },
+    "HACKING_HAMSTER": {  # different order than the FEATURE_FOX
+        # Magnetics new
+        "CEUS_MAG_TMAG_AAS_CEUSSSC_R0_Log": True,
+        "CEUS_MAG_DRTP_TDR_CEUSSSC_R0": True,
+        "CEUS_MAG_DRTP_HD_TDR_CEUSSSC_R0": True,
+        "CEUS_MAG_DRTP_CEUSSSC_R0": True,
+        # Gravity new
+        "CEUS_GRAV_RI_HD_1VD_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_HD_CEUSSSC_R0": True,
+        "CEUS_GRAV_Isostatic_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_CEUSSSC_R0": True,
+        "CEUS_GRAV_RI_1VD_CEUSSSC_R0": True,
+        # Things we have
+        "Moho": True,
+        "LAB": True,
+        "LAB_HGM": True,
+        "LAB_Worms_Proximity": True,
+        "Mag_RTP_HGM_DeepSources": True,
+        "DeepMagSources_Worms_Proximity": True,
+        # Geology
+        "distance_to_geology_macrostrat": True,
+        "distance_to_faults_macrostrat": True,
+        "cambrian-to-early-ordovician-carbonates_zero": True,
+        # Things to wait on: Geochemistry
+        "...": False,
+    },
 }
