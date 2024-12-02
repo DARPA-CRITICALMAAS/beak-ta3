b77ccd7ca101187777473ee12a75fdb91e1554bd
Michael Steffen <michael.steffen@beak.de>
2024-12-02 20:12:01 +0100

    install updates

---
+++ install/unix/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Function to log messages
+log_message() {
+    printf "%s\n" "$1"
+    printf "%s\n" "$1" >> "${SCRIPT_DIR}/setup.log"
+}
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "${SCRIPT_DIR}"
+
+log_message "Script started at $(date)"
+
+log_message "Creating Conda environment..."
+conda env create -f environment.yml --quiet
+
+log_message "Activating environment..."
+CONDA_BASE=$(conda info --base)
+source "$CONDA_BASE/etc/profile.d/conda.sh"
+conda activate beak-ta3
+
+log_message "Installing local package..."
+cd "${SCRIPT_DIR}/../.."
+pip install -e .
+
+log_message "Script finished at $(date)"
